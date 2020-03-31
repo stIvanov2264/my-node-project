720836785d1d2dd1df5dfbf3511e452128a9e995
stIvanov2264 <stanislav.s.ivanov@bg.ibm.com>
2020-03-31 13:09:06 +0300

    initial commit

---
+++ Hello.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+http.createServer(function (req,res) {
+	var q = url.parse(req.url, true);
+	var filename = "." + q.pathname;
+	if (filename == './') {filename = './index';}
+
+	filename = filename + ".html";
+	console.log(filename);
+	fs.readFile(filename, function(err, data) {
+		if (err) {
+			res.writeHead(404, {'content-type': 'text/html'});
+			return res.end ("404 Not found");
+		}
+    	res.writeHead(200, {'content-type': 'text/html'});
+		res.write(data);
+		return res.end();
+	});
+
+}).listen(8090);
+
+console.log("Server listening on port 8090...");
+
+
+
+
